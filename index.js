01baec643c3dc522064e7af270d4df875bf3ec0e
Anton M <mariadasanton27@gmail.com>
2021-12-02 19:55:28 +0100

    ajout variable env port

--- index.js
+++ index.js
@@ -5,11 +5,12 @@ const bodyParser = require('body-parser');
 require('./src/config/dbConfig');
 const companiesRoutes = require('./src/routes/companies');
 
-const port = 8383;
+
+const port = PORT || 8383;
 app.use(cors());
 app.use(bodyParser.json());
 app.use('/api', companiesRoutes);
 
 app.listen(port, () => {
-    console.log(`listening on : http://localhost:${port}`);
+    console.log(`listening on : ${port}`);
 });

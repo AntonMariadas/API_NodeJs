bf50a7bd8f7b33c10af7ff306623e57a3d5613b6
Anton M <mariadasanton27@gmail.com>
2022-02-01 14:09:11 +0100

    retrait fichier config de .env

---
+++ src/config/dbConfig.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+
+const mongooseKey = process.env.MONGOOSE_KEY;
+const databaseName = process.env.DATABASE_NAME;
+const database = `mongodb+srv://${mongooseKey}@cluster0.uivcu.mongodb.net/${databaseName}?retryWrites=true&w=majority`;
+
+mongoose.connect(database, {
+    useNewUrlParser: true,
+});
+
+mongoose.connection
+    .once('open', () => console.log("Connected"))
+    .on('error', (error) => console.log("Your error", error));
+
